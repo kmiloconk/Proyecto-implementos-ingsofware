51ddeaea2acc41a62b90654c9aaed3acfbcbb7b0
LuisEspinoza-UBB <130384073+LuisEspinoza-UBB@users.noreply.github.com>
2023-05-22 13:10:48 -0400

    a

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -6,7 +6,7 @@ const autheMiddleware = require("../middlewares/authe.middleware");
 
 const router = express.Router();
 
-router.get("/", usuarioController.getUsers);
+router.get("/users", usuarioController.getUsers);
 router.post("/", autheMiddleware.isAdmin, usuarioController.createUser);
 router.get("/:id", usuarioController.getUserById);
 router.put("/:id", autheMiddleware.isAdmin, usuarioController.updateUser);
